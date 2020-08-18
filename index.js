f44a9f2567e842ea2f459f5c1229e582e35e59ff
Andrés Eduardo Lara Hidalgo <aelarah@pronaca.com>
2020-08-18 14:21:58 -0500

    First Commit Andy

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/postgres')
+const app = express()
+const port = 8080
+app.use(express.json());
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+app.get('/', (req, res) => res.json({message: 'test api'}))
+app.listen(port, () => console.log(`ejecutando api en puerto ${port}`))
